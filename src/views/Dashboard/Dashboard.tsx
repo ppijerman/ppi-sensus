1e5543d7be89a4a894c8bef0e679abaf8306a903
Azhar Rahadian <a.rahadian@tu-braunschweig.de>
2024-06-16 01:49:17 +0200

    chore: Add StatsMahasiswa and StatsDemografi components for displaying statistics in Dashboard

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -8,6 +8,7 @@ import { FederalStateContext } from "./FederalStateContext";
 import { GeoVis } from "./Geovis";
 import { PPICabangGraph } from "./PPICabangGraph";
 import { UserStatistics } from "./Statistics";
+import { StatsMahasiswa } from "./StatsMahasiswa";
 
 export const Dashboard: NextPage = () => {
   const federalState = useContext(FederalStateContext);
@@ -25,7 +26,8 @@ export const Dashboard: NextPage = () => {
     { name: "Belum Terverifikasi", count: adminData?.unverified },
     { name: "Info belum lengkap", count: adminData?.updated },
   ];
-  const stats = [
+  const statsDemografi = [{ name: "Jumlah Mahasiswa", count: adminData?.users }];
+  const statsMahasiswa = [
     { name: "Ausbildung", count: data?.vocation },
     { name: "Bachelor", count: data?.bachelor },
     { name: "Master", count: data?.master },
@@ -62,6 +64,18 @@ export const Dashboard: NextPage = () => {
                   <UserStatistics stats={statsAdmin} />
                 </Card>
               </Protected>
+
+              <Card className="basis-full">
+                <UserStatistics stats={statsDemografi} />
+              </Card>
+              <Card className="basis-full md:basis-[300px]">
+                <StatsMahasiswa stats={statsMahasiswa} />
+              </Card>
+              {!federalState && (
+                <Card className="basis-full md:basis-[calc(100%-320px)]">
+                  <PPICabangGraph ppiCabangStats={ppiCabangStats} />
+                </Card>
+              )}
               <Card className="flex basis-full md:basis-[300px]">
                 <div className="h-[300px] w-full">
                   <ResponsiveContainer>
@@ -82,14 +96,6 @@ export const Dashboard: NextPage = () => {
                   </ResponsiveContainer>
                 </div>
               </Card>
-              <Card className="basis-full md:basis-[calc(100%-320px)]">
-                <UserStatistics stats={stats} />
-              </Card>
-              {!federalState && (
-                <Card className="basis-full">
-                  <PPICabangGraph ppiCabangStats={ppiCabangStats} />
-                </Card>
-              )}
             </div>
             <Card className="flex flex-col items-center 2xl:basis-1/3">
               <h1 className="mb-5 text-2xl font-semibold">
