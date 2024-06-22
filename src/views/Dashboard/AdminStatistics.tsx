573f7be6c8b22bc410878c4c5ef59dc55f0ed622
Remisys <yosefiankurniadi@gmail.com>
2024-06-22 19:57:27 +0200

    Changed one more label

--- src/views/Dashboard/AdminStatistics.tsx
+++ src/views/Dashboard/AdminStatistics.tsx
@@ -17,7 +17,9 @@ export const AdminStatistics: FC = () => {
   return (
     <>
       <hr className="my-10 w-full " />
-      <h1 className="text-3xl font-bold text-gray-900">Admin Statistics</h1>
+      <h1 className="text-3xl font-bold text-gray-900">
+        Statistik untuk Admin
+      </h1>
       <div className="mt-4 flex w-full flex-col gap-10 2xl:flex-row">
         <div className="flex flex-wrap justify-between gap-x-5 gap-y-5">
           <Card className="basis-full">
